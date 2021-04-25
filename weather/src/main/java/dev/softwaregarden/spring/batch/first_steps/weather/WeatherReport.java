12d0afa4bbc7bded75fbf53248ea53afff058269
Piotr <piotr@przybyl.org>
2021-04-25 19:05:35 +0200

    Added foundation for WeatherReport job.

---
+++ weather/src/main/java/dev/softwaregarden/spring/batch/first_steps/weather/WeatherReport.java
@@ -0,0 +1,62 @@
+/*
+ *
+ *  Copyright (C) 2021 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package dev.softwaregarden.spring.batch.first_steps.weather;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@Configuration
+public class WeatherReport {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public WeatherReport(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherReport.class, args);
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("weatherReportJob")
+            .incrementer(new RunIdIncrementer())
+            .start(stepBuilderFactory.get("dummy").tasklet((contribution, chunkContext) -> {
+                System.out.println("runs!");
+                return RepeatStatus.FINISHED;
+            }).build())
+            .build();
+    }
+
+
+}
